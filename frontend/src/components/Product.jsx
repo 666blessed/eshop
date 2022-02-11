bab196876435ec7d3cead908bc716a2590d5dd9c
Kirill Katkov <katkov.kirill10@gmail.com>
2022-02-11 17:54:40 +0200

    rating component added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 function Product({ product }) {
   return (
@@ -16,12 +17,15 @@ function Product({ product }) {
         </a>
 
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
 
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3' style={{ color: '#3d3d3d' }}>
+          <strong>${product.price}</strong>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
