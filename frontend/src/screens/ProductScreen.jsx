99f8b6e71fc995ebc5d0681c3069171bbdb729e5
Kirill Katkov <katkov.kirill10@gmail.com>
2022-02-11 18:18:00 +0200

    implemented basic routing with react router dom

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function ProductScreen() {
+  return (
+    <main className='py-3'>
+      <Container>
+        <div>ProductScreen</div>
+      </Container>
+    </main>
+  );
+}
+
+export default ProductScreen;
