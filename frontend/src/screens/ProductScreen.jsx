f61ded1b9e99fdbc865eb569856860943a57c69a
Kirill Katkov <katkov.kirill10@gmail.com>
2022-02-12 12:57:30 +0200

    implemented fetching products from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Row,
@@ -10,11 +10,20 @@ import {
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { Link, useParams } from 'react-router-dom';
-import products from '../products';
+import axios from 'axios';
 
 function ProductScreen({ match }) {
   const { id } = useParams();
-  const product = products.find(p => p._id === id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, []);
 
   return (
     <main className='py-3'>
