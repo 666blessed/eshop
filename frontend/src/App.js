7f50344d46152eb77949257711ee25028c7fc9bf
Kirill Katkov <katkov.kirill10@gmail.com>
2022-02-17 18:55:34 +0200

    started working on cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomeScreen />} />
         <Route path='/product/:id' element={<ProductScreen />} />
+        <Route path='/cart/:id' element={<CartScreen />} />
+        <Route path='/carto' element={<CartScreen />} />
       </Routes>
       <Footer />
     </Router>
